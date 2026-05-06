83fbdc76cf8d6c7dcf80ecd78531c478b459400e
Leandra Lemos <leandralemos960@gmail.com>
2026-05-06 20:25:47 -0300

    Fix verb forms in activities5.js

--- 05/activities5.js
+++ 05/activities5.js
@@ -1,43 +1,44 @@
 /* 
 A) Complete com o verbo correto:
 
-1. She works (work) yesterday.
+1. She worked (work) yesterday.
 
-2. They play (play) soccer last week.
+2. They played (play) soccer last week.
 
-3. He studies (study) English.
+3. He studied (study) English.
 
-4. I drink (drink) coffee.
+4. I fixed (fix) the bug.
 
 B) Transforme em negativa:
 
-1. She works → She doesn’t work
+1. She worked → She didn’t work
 
-2. He studies → He doesn’t study
+2. He studied → He didn’t study
 
-3. They play → They don’t play
+3. They played → They didn’t play
 
 C) Transforme em pergunta:
 
-1. You works → Do you work?
+1. You worked → Did you work?
 
-2. She studies → Does she study?
+2. She studied → Did she study?
 
-3. They play → Do they play?
+3. They played → Did they play?
 
 D) Corrija os erros:
 
-1. I did works → I did work (ou simplesmente: I works)
+1. I did worked → I did work (ou simplesmente: I worked)
 
-2. She didn't studies → She didn’t study
+2. She didn't studied → She didn’t study
 
-3. Did you works? → Did you work?
+3. Did you worked? → Did you work?
 
 E) Traduza:
 
-1. Eu trabalhei ontem → I works yesterday
+1. Eu trabalhei ontem → I worked yesterday
 
-2. Nós corrigimos o erro → We fixes the bug
+2. Nós corrigimos o erro → We fixed the bug
 
-3. Ela estudou inglês → She studies English
+3. Ela estudou inglês → She studied English
 */
+
