2ebc8d97cfa4e1a14f88030aeea9957c231a0aae
LEANDRA CRUZ DE LEMOS <04785634006@senacrs.edu.br>
2026-05-14 20:25:24 -0300

    Adicionei as atividades mais recentes

---
+++ 06/activities6.js
@@ -0,0 +1,20 @@
+/* 
+
+Criem 10 frases com present continuous
+Entre as 10 frases, deve conter interrogativo e negativo
+
+
+She is studing at the moment.
+Are they working?
+He isn't playing now.
+Is it running right now?
+We're thinking in make this for december.
+I am making the dinner.
+They aren't in home.
+Is she writing now?
+I am coding right now.
+Are you working?
+
+
+
+*/
---
+++ 07/activities07.js
@@ -0,0 +1,26 @@
+/*
+EXEMPLO
+SIMPLE PRESENT | PRESENT CONTINUOUS | QUESTION USING PRESENT CONTINUOUS
+I work.        | I am working.      | Am i working?
+
+It runs.       | It is running.      | Is it running?        
+They swim.     | They are swimming.  | Are they swimming?
+We sit.        | We are sitting.     | Are we sitting?
+You stop.      | You is stopping.    | Is you stopping?
+I plan.        | I am planning.      | Am i planning?
+They travel.   | They are traveling. | Are they traveling?
+We dance.      | We are dancing.     | Are we dancing?
+I write.       | I am writing.       | Am i writing?
+You make.      | You are making.     | Are you making?
+He drive.      | He is driving.      | Is he driving?
+She come.      | She is coming.      | Is she coming?
+He take.       | He is taking.       | Is he taking?
+She move.      | She is moving.      | Is she moving?
+He lie.        | He is lying.        | Is he lying?
+She get.       | She is getting.     | Is she getting?
+He shop.       | He is shopping.     | Is he shopping?
+She begin.     | She is beginning.   | Is she beginning?
+We forget.     | We are forgetting.  | Are we forgetting?
+They use.      | They are using.     | Are they using?
+I live.        | I am living.        | Am i living?
+*/
