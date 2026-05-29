2775a6c6b5e1377fe9dde704c8da81588677f54a
LEANDRA CRUZ DE LEMOS <04785634006@senacrs.edu.br>
2026-05-28 21:09:51 -0300

    Em progresso

---
+++ 04/activity02.js
@@ -0,0 +1,27 @@
+/*
+EXEMPLO
+SIMPLE PRESENT | SIMPLE PAST | PRESENT CONTINUOUS  | QUESTION USING PRESENT CONTINUOUS  
+I work.        |  I worked.  |    I am working.       | Am i working?
+
+It runs.       | It runed..     | It is running.      | Is it running?        
+They swim.     | They swimed.  | They are swimming.  | Are they swimming?
+We sit.        | We sited.     | We are sitting.     | Are we sitting?
+You stop.      | You stoped.   | You is stopping.    | Is you stopping?
+I plan.        | I planed.     | I am planning.      | Am i planning?
+They travel.   | They traveled.| They are traveling. | Are they traveling?
+We dance.      | We danced.    | We are dancing.     | Are we dancing?
+I write.       | I writed.     | I am writing.       | Am i writing?
+You make.      | You made.     | You are making.     | Are you making?
+He drive.      | He drived     | He is driving.      | Is he driving?
+She come.      | She came.     | She is coming.      | Is she coming?
+He take.       | He took.       |  He is taking.       | Is he taking?
+She move.      | She moved.     |   She is moving.    | Is she moving?
+He lie.        | He lied.      | He is lying.        | Is he lying?
+She get.       | She geted      | She is getting.     | Is she getting?
+He shop.       | He shoped.     | He is shopping.     | Is he shopping?
+She begin.     | She begined.  | She is beginning.   | Is she beginning?
+We forget.     | We forgeted.  | We are forgetting.  | Are we forgetting?
+They use.      | They used.    | They are using.     | Are they using?
+I live.        |  I lived.     |  I am living.       | Am i living?
+
+*/
---
+++ 06/activities06.js
@@ -0,0 +1,48 @@
+/* 
+A) Complete com o verbo correto
+
+1. I will work tomorrow
+
+2. She will study later
+
+3. They are going to deploy
+
+4. We will fix the bug
+
+B) Use "will" ou "going to"
+
+1. I will help you. (decisão agora)
+
+2. I am going to study tonight. (planejado)
+
+3. We will fix the problem. (decisão rápida)
+
+C) Transforme em negativa
+
+1. I will work → I will not (won’t) work.
+
+2. She is going to study → She is not going to study.
+
+D) Transforme em pergunta
+
+1. You will work → Will you work?
+
+2. She is going to study → Is she going to study?
+
+E) Corrija os erros
+
+1. I will worked → I will work.
+
+2. She is going to studying → She is going to study.
+
+3. I will working → I will work.
+
+F) Traduza
+
+1. Eu vou trabalhar amanhã → I will work tomorrow.
+
+2. Nós vamos corrigir o erro → We will fix the bug/error.
+
+3. Ela vai estudar depois → She will study later.
+
+*/
